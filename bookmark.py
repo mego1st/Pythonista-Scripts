a8c5f0c4547f943ea9b45e32a1097fe55d943088
mego1st <djmystic@hotmail.com>
2020-04-14 19:44:26 -0500

    Update bookmark.py

--- bookmark.py
+++ bookmark.py
@@ -3,7 +3,7 @@ import appex
 url = appex.get_web_page_info()
 print(url['url'])
 
-def write_to_file:
+def write_to_file():
   file = open('bookmarks.txt', 'a+')
   a = (url['url'])
   file.write(a + '\n')
