a76b9ebd85ae2495b12c598fc053802d0d70fcc6
mego1st <djmystic@hotmail.com>
2020-04-14 20:00:19 -0500

    Update bookmark.py

--- bookmark.py
+++ bookmark.py
@@ -1,12 +1,14 @@
 import appex
 
-url = appex.get_web_page_info()
-print(url['url'])
-
-def write_to_file():
+def main():
+  #retrieve url when ran in safari
+  url = appex.get_web_page_info()
+  print(url['url'])
+  #write url to file
   file = open('bookmarks.txt', 'a+')
   a = (url['url'])
   file.write(a + '\n')
   file.close()
 
-write_to_file()
+if __name__ == '__main__':
+  main()
